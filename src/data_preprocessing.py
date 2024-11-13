2747648ab1b9d2e8c787fd02b3dfab2aa88a2532
DataAIOPS <shubhkalpatil122@gmail.com>
2024-11-13 22:00:09 +0530

    first

---
+++ src/data_preprocessing.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import yaml
+
+with open("params.yaml") as f:
+    params = yaml.safe_load(f)
+
+def load_data(path):
+    return pd.read_csv(path)
+
+
+def preprocess(data):
+    ## preprocessing steps
+    return data
+
+if __name__ == "__main__":
+    data = load_data(params["data_path"])
+    processed_data = preprocess(data)
+
+    processed_data.to_csv(params["processed_data_path"],index=False)
